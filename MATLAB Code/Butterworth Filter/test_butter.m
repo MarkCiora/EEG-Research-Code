42f23f80d250c3be8d628eb74e2d08880c84fbcc
markciora <mfc32@pitt.edu>
2021-05-07 08:45:11 -0400

    side-by-side comparison of my butter and MATLAB filter

--- MATLAB Code/Butterworth Filter/test_butter.m
+++ MATLAB Code/Butterworth Filter/test_butter.m
@@ -53,3 +53,94 @@ title('My Filter')
 %hold on;
 %plot(t, delta - test);
 %hold off;
+
+%% Side-by-side comparison of my_butter and MATLAB function
+%% Unfiltered
+
+N = 1; % order = 2N;
+norm = 1/(Fs/2);
+
+% plot unfiltered EEG signal
+figure(1)
+plot(t,y)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Unfiltered Sleep EEG Signal')
+legend(strcat("Record ",int2str(rec),", Signal ",info.SignalLabels(sig)))
+
+%% MATLAB
+% extract and plot 
+figure(2)
+sgtitle('Second Order Butterworth Filtered Sleep Signal MATLAB')
+
+subplot(411)
+[b,a] = butter(N, [1*norm 4*norm], 'bandpass');
+delta = filter(b, a, y);
+
+plot(t, delta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Delta Component')
+
+subplot(412)
+[b,a] = butter(N, [4*norm 8*norm], 'bandpass');
+theta = filter(b, a, y);
+
+plot(t, theta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Theta Component')
+
+subplot(413)
+[b,a] = butter(N, [8*norm 12*norm], 'bandpass');
+alpha = filter(b, a, y);
+
+plot(t, alpha)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Alpha Component')
+
+subplot(414)
+[b,a] = butter(N, [12*norm 35*norm], 'bandpass');
+beta = filter(b, a, y);
+
+plot(t, beta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Beta Component')
+
+%% my_butter
+figure(3)
+sgtitle('Second Order Butterworth Filtered Sleep Signal my butter')
+
+subplot(411)
+delta = my_butter(1,4,Fs/2,y);
+
+plot(t, delta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Delta Component')
+
+subplot(412)
+theta = my_butter(4,8,Fs/2,y);
+
+plot(t, theta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Theta Component')
+
+subplot(413)
+alpha = my_butter(8,12,Fs/2,y);
+
+plot(t, alpha)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Alpha Component')
+
+subplot(414)
+beta = my_butter(12,35,Fs/2,y);
+
+plot(t, beta)
+xlabel('t (in sec)')
+ylabel('\muV')
+title('Beta Component')
